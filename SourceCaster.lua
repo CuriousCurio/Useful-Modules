63c777216b17bf029777045ffda69ed8c06ad4e4
CuriousCurio <116847106+CuriousCurio@users.noreply.github.com>
2022-11-11 15:44:57 -0700

    Update SourceCaster.lua

--- SourceCaster.lua
+++ SourceCaster.lua
@@ -1,6 +1,6 @@
 -- This module formats datatypes into initializers for use in source code. Datatypes will be changed into neat string initializers
 
-local encode_types:{} = require(script.EncodeTypes);
+local encode_types:{} = require(script.Parent.EncodeTypes);
 
 local module = {};
 function module:Encode(v:any)
